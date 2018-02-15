f6cd0ad08bd512659089894932f2bf7989e96cf5
William Ricardo Ariza Velez <william.ariza@arandasoft.com>
2018-02-14 20:29:08 -0500

    ajuste en las rutas para deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,66 @@
-const path = require('path');
+const webpack = require("webpack");
+const path = require('path'); 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env) => {
   const plugins = [
-    new ExtractTextPlugin("css/[name].css")
+    new ExtractTextPlugin("css/bundle.css"),
   ]
 
   if (env.NODE_ENV === 'production') {
     plugins.push(
-      new CleanWebpackPlugin(['dist'], {root: __dirname})
+      new CleanWebpackPlugin(['dist'], {root: __dirname}),
     )
-    
   }
 
   return {
-
     entry: {
-      youse: path.resolve(__dirname, 'src/app.js'),
+      app: [path.resolve(__dirname, 'src/app.js')],
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'js/[name].js',
-      publicPath: path.resolve(__dirname, 'dist')+"/",
-      chunkFilename: 'js/[id].js',
+      publicPath: path.resolve(__dirname, 'dist')+"/"
     },
     devServer: {
-      port: 9000,
+        contentBase: path.join(__dirname, "dist"),
+        compress: false,
+        port: 9000,
     },
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['es2015', 'react', 'stage-2'],
+        rules: [
+          {
+            test: /\.(css|sass|scss)$/,
+            use: ExtractTextPlugin.extract({ 
+              fallback: 'style-loader', 
+              use: 'css-loader!sass-loader' 
+            })
+          },
+          {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['es2015', 'react', 'stage-2'],
+              }
             }
           },
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
+          {
+            test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
             use: [
               {
-                loader: 'css-loader',
+                loader: 'url-loader',
                 options: {
-                  minimize: true,
+                  limit: 100000
                 }
               }
             ]
-          })
-        },
-        {
-          test: /\.(jpg|png|gif|svg)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              fallback: 'file-loader',
-              name: 'images/[name].[ext]',
-            }
           }
-        },
-      ]
-    },
-    plugins
+        ]
+      },
+      plugins
+    }
   }
-}
